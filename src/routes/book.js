060ab7bf5bd481ea93b05ca4d6b7511cca037d0b
Miranda <943507@cognizant.com>
2021-07-28 16:17:21 -0400

    hopefully I fixed it

--- src/routes/book.js
+++ src/routes/book.js
@@ -1,8 +1,8 @@
 var express = require('express');
 const Book = require('../models/books');
 var router = express.Router();
-var MongoClient = require('../mongoConfig/booksConnection');
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.yk4je.mongodb.net/payper?retryWrites=true&w=majority`;
+var client = require('../mongoConfig/booksConnection');
+
 // let books = [
 //   {title: "The Lord of the Rings", subtitle: "The Fellowship of the Ring", author:"J.R.R. Tolkien", price: 10.99, isbn: 9780007203581, copies: 2, img:"https://images-na.ssl-images-amazon.com/images/I/51tW-UJVfHL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"},
 //   {title: "The Lord of the Rings", subtitle: "The Two Towers", author: "J.R.R. Tolkien", price: 12.99, isbn: 9780007203598, copies: 3, img: "https://m.media-amazon.com/images/I/71u8+yoKy-L._AC_SX960_SY720_.jpg"},
@@ -11,8 +11,7 @@ const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}
 
 /* GET books listing. */
 router.get('/', function(req, res, next) {
-  MongoClient.connect(err =>{
-    if(err){throw err};
+  client.connect(err =>{
     const collection = client.db("payper").collection("books");
     collection.find().toArray((err,books)=>{
 
