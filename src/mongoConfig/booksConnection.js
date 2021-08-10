997f93deafbc5ec4cd94e81505b7a91a69fa3e66
joemface <joemface@gmail.com>
2021-08-10 15:46:29 -0400

    adjusted connection string

--- src/mongoConfig/booksConnection.js
+++ src/mongoConfig/booksConnection.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 const dotenv = require('dotenv');
 
 dotenv.config();
-const uri = `mongodb+srv://${process.env.DB_STRING}@cluster0.yk4je.mongodb.net/payper?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://"+process.env.DB_STRING+"@cluster0.yk4je.mongodb.net/payper?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = client;
