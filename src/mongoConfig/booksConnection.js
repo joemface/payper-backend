39f7744a24ff21e6db3d298371ff6e760053d0e5
joemface <joemface@gmail.com>
2021-08-10 16:24:06 -0400

    commented out dotenv

--- src/mongoConfig/booksConnection.js
+++ src/mongoConfig/booksConnection.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
-const dotenv = require('dotenv');
+//const dotenv = require('dotenv');
 
-dotenv.config();
+//dotenv.config();
 const uri = "mongodb+srv://"+process.env.DB_STRING+"@cluster0.yk4je.mongodb.net/payper?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
