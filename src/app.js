6b7b18a60f690bbfee6c7525de713b5882490a91
joemface <joemface@gmail.com>
2021-08-12 19:21:16 -0400

    tested localhost on google

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+
+//routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var booksRouter = require('./routes/book');
@@ -13,8 +15,8 @@ var booksRouter = require('./routes/book');
 var app = express();
 
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,16 +30,10 @@ app.use(cors());
 app.use('/', booksRouter);
 app.use('/books', booksRouter);
 
-//app.use('/users', usersRouter);
-//app.use('/checkout',usersRouter);
-//app.use('/shopping-cart',usersRouter);
-
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  //next(createError(404));
+  next(createError(404));
 });
 
 // error handler
--- src/routes/book.js
+++ src/routes/book.js
@@ -3,22 +3,11 @@ const Book = require("../models/books");
 var router = express.Router();
 var client = require("../mongoConfig/booksConnection");
 
-// let books = [
-//   {title: "The Lord of the Rings", subtitle: "The Fellowship of the Ring", author:"J.R.R. Tolkien", price: 10.99, isbn: 9780007203581, copies: 2, img:"https://images-na.ssl-images-amazon.com/images/I/51tW-UJVfHL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"},
-//   {title: "The Lord of the Rings", subtitle: "The Two Towers", author: "J.R.R. Tolkien", price: 12.99, isbn: 9780007203598, copies: 3, img: "https://m.media-amazon.com/images/I/71u8+yoKy-L._AC_SX960_SY720_.jpg"},
-//   {title: "The Lord of the Rings", subtitle: "The Return of the King", author: "J.R.R. Tolkien", price: 6.99, isbn: 9788845270758, copies: 5, img: "https://images-na.ssl-images-amazon.com/images/I/51MlPWDaXGL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"},
-// ];
-
-
-
-/* GET books listing. */
 router.get("/",  (req, res) => {
   client.connect(() => {
     const collection = client.db("payper").collection("books");
     collection.find({}).toArray(function (err, books) {
-
-      res.send(books);
-
+      res.send(JSON.stringify(books));
       client.close();
     });
   });
