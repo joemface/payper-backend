319c99771b6d20f8655bd4dafd981426de1a81ca
joemface <joemface@gmail.com>
2021-08-13 00:31:15 -0400

    please work aws

--- src/mongoConfig/booksConnection.js
+++ src/mongoConfig/booksConnection.js
@@ -1,6 +1,6 @@
+const { MongoClient } = require('mongodb');
 const dotenv = require('dotenv');
 dotenv.config();
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://"+process.env.DB_STRING+"@cluster0.yk4je.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://"+DB_STRING+"@paypercluster.qww7n.mongodb.net/payper?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 module.exports = client;
