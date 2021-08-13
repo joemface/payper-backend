799a783e5412749d93bd48927a2ad41560c5d14e
joemface <joemface@gmail.com>
2021-08-12 22:59:23 -0400

    rearranged some code

--- src/mongoConfig/booksConnection.js
+++ src/mongoConfig/booksConnection.js
@@ -1,8 +1,6 @@
-const { MongoClient } = require('mongodb');
 const dotenv = require('dotenv');
-
 dotenv.config();
-const uri = `mongodb+srv://${process.env.DB_STRING}@cluster0.yk4je.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://"+process.env.DB_STRING+"@cluster0.yk4je.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
 module.exports = client;
